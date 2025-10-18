d74b231e359e1b3705d3758dff4904c2d1df9b1e
Bodrogi Ádám Brúnó <bodrogiadam@Bodrogi-Mac-mini.local>
2025-10-18 19:46:39 +0200

    Fix unsplash import syntax

--- lib/unsplash.ts
+++ lib/unsplash.ts
@@ -1,4 +1,4 @@
-const { createApi } = require('unsplash-js')
+import { createApi } from 'unsplash-js'
 
 export const unsplash = createApi({
   accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY || '',
